6be1bb8ecb30664e2c3457991fb62f83de633bb3
Your Name <you@example.com>
2023-12-09 22:24:13 -0500

    optimize booking form

--- components/BookingForm.js
+++ components/BookingForm.js
@@ -11,14 +11,16 @@ const BookingForm = ({ onBookingAdded }) => {
     });
 
     // Load clients and shows data
-    const [clients, setClients] = useState(loadData('clients') || []);
-    const [shows, setShows] = useState(loadData('shows') || []);
+    const [clients, setClients] = useState([]);
+    const [shows, setShows] = useState([]);
 
     useEffect(() => {
-        // Preload clients and shows, if not already loaded
-        if (!clients.length) setClients(loadData('clients') || []);
-        if (!shows.length) setShows(loadData('shows') || []);
-    }, [clients.length, shows.length]);
+        // Loading clients and shows data in useEffect to avoid server/client mismatch
+        const loadedClients = loadData('clients') || [];
+        const loadedShows = loadData('shows') || [];
+        setClients(loadedClients);
+        setShows(loadedShows);
+    }, []);
 
     const handleChange = (e) => {
         setBooking({ ...booking, [e.target.name]: e.target.value });
@@ -29,10 +31,11 @@ const BookingForm = ({ onBookingAdded }) => {
         setBooking({ ...booking, [name]: value });
 
         if (name === 'endDate' || (name === 'startDate' && booking.endDate)) {
-            const start = new Date(booking.startDate + 'T00:00:00Z');
-            const end = new Date(value + 'T00:00:00Z');
-            const diffDays = (end - start) / (1000 * 60 * 60 * 24) + 1;
-            setBooking(prev => ({ ...prev, agentCounts: Array(diffDays).fill(prev.agentCounts[0] || 0) }));
+            // Regenerate agentCounts with correct dates when startDate or endDate changes
+            const start = new Date(booking.startDate);
+            const end = new Date(value);
+            const newAgentCounts = generateDateRange(start, end).map((_, index) => booking.agentCounts[index] || 0);
+            setBooking(prev => ({ ...prev, agentCounts: newAgentCounts }));
         }
     };
 
@@ -46,7 +49,7 @@ const BookingForm = ({ onBookingAdded }) => {
         e.preventDefault();
         const newBooking = {
             ...booking,
-            id: `${booking.show}-${new Date().getTime()}`, // Generate a unique ID for the booking
+            id: `${booking.show}-${booking.client}`, // Generate a unique ID for the booking
             totalDays: booking.agentCounts.reduce((acc, val) => acc + val, 0),
         };
 
@@ -56,21 +59,37 @@ const BookingForm = ({ onBookingAdded }) => {
         setBooking({ show: '', client: '', startDate: '', endDate: '', agentCounts: [] });
     };
 
+    // This function generates a range of dates between two dates
+    const generateDateRange = (start, end) => {
+        let dates = [];
+        let currentDate = new Date(start.toISOString().split('T')[0]); // Remove the time part
+        let endDate = new Date(end.toISOString().split('T')[0]);
+
+        while (currentDate <= endDate) {
+            dates.push(new Date(currentDate));
+            currentDate.setDate(currentDate.getDate() + 1);
+        }
+
+        return dates;
+    };
+
     return (
         <div className="form-container">
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="show">Show:</label>
-                    <select id="show" name="show" value={booking.show} onChange={handleChange}>
-                        {shows.map(show => <option key={show.id} value={show.id}>{show.id}</option>)}
-                    </select>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="client">Client:</label>
-                    <select id="client" name="client" value={booking.client} onChange={handleChange}>
-                        {clients.map(client => <option key={client.id} value={client.company}>{client.company}</option>)}
-                    </select>
-                </div>
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label htmlFor="show">Show:</label>
+                <select id="show" name="show" value={booking.show} onChange={handleChange}>
+                    <option value="" disabled selected>Select a show</option>
+                    {shows.map(show => <option key={show.id} value={show.id}>{show.id}</option>)}
+                </select>
+            </div>
+            <div className="form-group">
+                <label htmlFor="client">Client:</label>
+                <select id="client" name="client" value={booking.client} onChange={handleChange}>
+                    <option value="" disabled selected>Select a client</option>
+                    {clients.map(client => <option key={client.id} value={client.company}>{client.company}</option>)}
+                </select>
+            </div>
                 <div className="form-group">
                     <label htmlFor="startDate">Start Date:</label>
                     <input type="date" id="startDate" name="startDate" value={booking.startDate} onChange={handleDateChange} />
@@ -79,20 +98,21 @@ const BookingForm = ({ onBookingAdded }) => {
                     <label htmlFor="endDate">End Date:</label>
                     <input type="date" id="endDate" name="endDate" value={booking.endDate} onChange={handleDateChange} />
                 </div>
-                {booking.agentCounts.map((count, index) => (
-                    <div key={index} className="form-group">
-                        <label htmlFor={`agentCount-${index}`}>
-                            Agents on {new Date(booking.startDate).toISOString().slice(0, 10)}:
-                        </label>
-                        <input
-                            type="number"
-                            id={`agentCount-${index}`}
-                            min="0"
-                            value={count}
-                            onChange={(e) => handleAgentCountChange(index, e.target.value)}
-                        />
-                    </div>
-                ))}
+                {booking.startDate && booking.endDate && generateDateRange(new Date(booking.startDate), new Date(booking.endDate)).map((date, index) => (
+    <div key={index} className="form-group">
+        <label htmlFor={`agentCount-${index}`}>
+            Agents on {date.toISOString().slice(0, 10)}:
+        </label>
+        <input
+            type="number"
+            id={`agentCount-${index}`}
+            name={`agentCount-${index}`}
+            min="0"
+            value={booking.agentCounts[index] || 0}
+            onChange={(e) => handleAgentCountChange(index, e.target.value)}
+        />
+    </div>
+))}
                 <button type="submit" className="button">Add Booking</button>
             </form>
         </div>
