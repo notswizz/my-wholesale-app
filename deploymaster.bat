6aa637e18355e536dd8d8c830edbbf0e0063b035
Your Name <you@example.com>
2023-12-12 16:31:59 -0500

    deploymaster script update

--- deploymaster.bat
+++ deploymaster.bat
@@ -1,7 +1,16 @@
 @echo off
+echo Switching to master branch...
+git checkout master
+
+echo Pulling latest changes from master...
+git pull origin master
+
 SET /P COMMITMESSAGE="Enter commit message: "
 git add .
 git commit -m "%COMMITMESSAGE%"
+
+echo Pushing changes to master branch on GitHub...
 git push origin master
+
 echo Changes pushed to GitHub.
 pause
