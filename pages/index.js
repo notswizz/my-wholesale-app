e9756672eaf027cddb6839a3747bbbccefb05791
Your Name <you@example.com>
2023-12-12 13:24:11 -0500

    landing page img resize

--- pages/index.js
+++ pages/index.js
@@ -18,14 +18,11 @@ const HomePage = () => {
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
          
           <div style={{ flex: 1, marginLeft: '10px', textAlign: 'center' }}>
-            <Image src="/tsalogoai.png" alt="TSA Logo" width={500} height={500} />
-          </div>
-          <div style={{ flex: 1, marginLeft: '10px', textAlign: 'center' }}>
             <Image src="/tsalogoai2.png" alt="TSA Logo" width={500} height={500} />
           </div>
           <div style={{ flex: 1, marginLeft: '10px' }}>
             {/* Instagram Embed Block */}
-            <blockquote className="instagram-media" data-instgrm-permalink="https://www.instagram.com/the_smithagency/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" style={{ background: '#FFF', border: '0', borderRadius: '3px', boxShadow: '0 0 1px 0 rgba(0,0,0,0.5), 0 1px 10px 0 rgba(0,0,0,0.15)', margin: '1px', maxWidth: '540px', minWidth: '326px', padding: '0', width: '99.375%', width: '-webkit-calc(100% - 2px)', width: 'calc(100% - 2px)' }}>
+            <blockquote className="instagram-media" data-instgrm-permalink="https://www.instagram.com/the_smithagency/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" style={{ background: '#FFF', border: '0', borderRadius: '3px', boxShadow: '0 0 1px 0 rgba(0,0,0,0.5), 0 1px 10px 0 rgba(0,0,0,0.15)', margin: '1px', maxWidth: '450px', minWidth: '375px', padding: '0', width: '99.375%', width: '-webkit-calc(100% - 2px)', width: 'calc(100% - 2px)' }}>
               {/* The rest of the provided Instagram embed code goes here */}
             </blockquote>
           </div>
